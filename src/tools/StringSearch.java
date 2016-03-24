6e729f6cc952da5184e72d089d01aa9c581c0040
arne <arne@mw171175.med.rug.nl>
2016-03-24 10:38:32 +0100

    StringSearch commented

--- src/tools/StringSearch.java
+++ src/tools/StringSearch.java
@@ -12,7 +12,7 @@ public class StringSearch {
     /**
      * The bad character skip array.
      */
-    private int[] right;
+    private int[] shifts;
     /**
      * The radix.
      */
@@ -26,19 +26,19 @@ public class StringSearch {
      * Constructor of the class, sets all used and preprocesses the pattern
      * string.
      *
-     * @param pat The string to search for
+     * @param patternIn The string to search for
      */
-    public StringSearch(final String pat) {
+    public StringSearch(final String patternIn) {
         this.R = 256; // set the radix
-        this.pattern = pat; // set the pattern
-        right = new int[R]; // create the bad character skip array
+        this.pattern = patternIn; // set the pattern
+        shifts = new int[R]; // create the bad character skip array
         // sets all to -1
-        for (int c = 0; c < R; c++) {
-            right[c] = -1;
+        for (int i = 0; i < R; i++) {
+            shifts[i] = -1;
         }
         // sets the position of the rightmost occurrence of a character in the pattern
-        for (int j = 0; j < pat.length(); j++) {
-            right[pat.charAt(j)] = j;
+        for (int j = 0; j < patternIn.length(); j++) {
+            shifts[patternIn.charAt(j)] = j;
         }
     }
 
@@ -55,7 +55,7 @@ public class StringSearch {
             skip = 0;
             for (int j = patLen - 1; j >= 0; j--) {
                 if (pattern.charAt(j) != stringIn.charAt(i + j)) {
-                    skip = Math.max(1, j - right[stringIn.charAt(i + j)]);
+                    skip = Math.max(1, j - shifts[stringIn.charAt(i + j)]);
                     break;
                 }
             }
